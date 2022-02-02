ea662d07e5471cfe2578a692079fd2747da3426b
anduagu <andres.agudelo.m22@gmail.com>
2022-02-02 13:54:09 -0500

    esta si si

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/graduation-form'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/graduation-form/index.html'));});
+
+app.use(express.static(__dirname+'/dist/ng-blog'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/graduation-form/index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
