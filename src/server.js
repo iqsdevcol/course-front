96c5ded2845df059799d784f9fa334566dc199ba
anduagu <andres.agudelo.m22@gmail.com>
2022-02-02 20:43:43 -0500

    esta si x51

--- src/server.js
+++ src/server.js
@@ -1,21 +1,8 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/graduation-form'));
-
+app.use(express.static(__dirname + '/dist/graduation-form'));
 app.get('/*', function(req,res) {
-
-  if(req.url === '/') {
-    res.sendFile(path.join(__dirname,'/dist/graduation-form/index.html'));
-  }
-  else {
-    res.sendFile(path.join(__dirname, '/dist/graduation-form' + req.url))
-  }
-
-});
-
-// Start the app by listening on the default Heroku port
+res.sendFile(path.join(__dirname+
+'/dist/graduation-form/index.html'));});
 app.listen(process.env.PORT || 8080);
