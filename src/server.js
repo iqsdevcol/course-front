bf4861ef6b5c8cbffdcd8d08a611d3f185b1458e
anduagu <andres.agudelo.m22@gmail.com>
2022-02-02 21:25:38 -0500

    esta si

--- src/server.js
+++ src/server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static('/app/dist/graduation-form'));
 app.listen(process.env.PORT || 8080);
 
 //PATH LOCATION STARTEGY
 
 app.get('/*', function(req,res){
-  const fullPath = path.join(__dirname + '/index.html');
+  const fullPath = path.join('/app/dist/graduation-form/index.html');
   console.log(" Fetching from.." + fullPath);
     res.sendFile(fullPath);
 })
